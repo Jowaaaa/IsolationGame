bea4d49714d087bea08728f03a2aa53df55b23dd
Jowaaaa <jobvdw.jvdw@gmail.com>
2023-03-09 11:43:41 +0100

    const

--- IsolationGame.py
+++ IsolationGame.py
@@ -3,25 +3,8 @@ import random
 import pygame
 import sys
 import math
+from const.settings import *
 
-## Instellingen ##
-
-Row_Number = 4
-Column_Number = 4
-Square_Size = 100
-Radius = int(Square_Size/2 - 12)
-notVisited = 0
-visited  = 1
-PlayerPiece = 0
-AIPiece = 1
-
-BLUE = (0,0,255)
-RED = (255,0,0)
-BLACK = (0,0,0)
-GREY = (128,128,128)
-WHITE = (255,255,255)
-
-## Instellingen ##
 
 def isValidLocation(board,row,col,myPosition,opponentPosition):
     r,c = myPosition
---
+++ const/settings.py
@@ -0,0 +1,14 @@
+Row_Number = 4
+Column_Number = 4
+Square_Size = 100
+Radius = int(Square_Size/2 - 12)
+notVisited = 0
+visited  = 1
+PlayerPiece = 0
+AIPiece = 1
+
+BLUE = (0,0,255)
+RED = (255,0,0)
+BLACK = (0,0,0)
+GREY = (128,128,128)
+WHITE = (255,255,255)
